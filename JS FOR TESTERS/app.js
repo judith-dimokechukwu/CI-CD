6d8d7cbdfc5f0254dc81758f76fb9a667d575835
Judith Dimokechukwu <dimokechukwu.judith@barnksfortetech.com>
2026-05-10 17:17:37 +0100

    First-Function

--- JS FOR TESTERS/app.js
+++ JS FOR TESTERS/app.js
@@ -1,5 +1,10 @@
-for(let number = 1; number<=20; number = number+1){
-if(number % 2 !==0){
-    console.log (number +' is an odd number')
+// for(let number = 1; number<=20; number = number+1){
+// if(number % 2 !==0){
+//     console.log (number +' is an odd number')
+// }
+// }
+function calculateAreaOfRectangle(length, width) {
+    let area = length * width
+    console.log(area)
 }
-}
+calculateAreaOfRectangle(5, 3)
