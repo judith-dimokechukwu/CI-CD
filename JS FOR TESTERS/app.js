9d1a74ef9443d77cbe67425f5bd98b5fcf7ceaeb
Judith Dimokechukwu <dimokechukwu.judith@barnksfortetech.com>
2026-05-10 18:13:20 +0100

    Friends-Array

--- JS FOR TESTERS/app.js
+++ JS FOR TESTERS/app.js
@@ -3,9 +3,11 @@
 //     console.log (number +' is an odd number')
 // }
 // }
-function calculateAreaOfRectangle(length, width) {
-    let area = length * width
-    return area
-}
-let areaOfRectangle = calculateAreaOfRectangle(5, 6)
-console.log(areaOfRectangle)
+// function calculateAreaOfRectangle(length, width) {
+//     let area = length * width
+//     return area
+// }
+// let areaOfRectangle = calculateAreaOfRectangle(5, 6)
+// console.log(areaOfRectangle)
+const myBestFriends = ['Blessing', "Chizzy","Amara", 'Dera', 'Lynda']
+console.log(myBestFriends)
